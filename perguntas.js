002d5793e70dc9384336bc0704d4444f7fa724af
AbraaoTecnologiaAlura <00001117056934SP@al.educacao.sp.gov.br>
2024-11-05 09:06:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'sociedade',
+    'quando e´o enem?',
+    'O enem em feito em começo do mes de novembro'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Tchau em Inglês?',
+    'Oi em ingles é Bye (by)'
+)
